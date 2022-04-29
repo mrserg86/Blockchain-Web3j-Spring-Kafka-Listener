2ab5bfa8f34d356ba9ce00498e624ff00b8c847d
mrserg86 <skrylov@blockwit.io>
2022-04-29 13:25:04 +0300

    Initial commit

---
+++ src/main/java/com/mrserg86/EventsOfSmartContract/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.mrserg86.EventsOfSmartContract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mrserg86/EventsOfSmartContract/controllers/MainController.java
@@ -0,0 +1,10 @@
+package com.mrserg86.EventsOfSmartContract.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+
+
+}
