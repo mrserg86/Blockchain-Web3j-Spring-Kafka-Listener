ec2e66eb9c9cd2a8e95c22be5a68ed5b1afa883d
mrserg86 <skrylov@blockwit.io>
2023-03-19 13:08:33 +0300

    Work just for one address, consumer doesn't see several messages with addresses. Find a way

--- src/main/java/com/mrserg86/EventsOfSmartContract/JavaKafkaConsumerExample.java
+++ src/main/java/com/mrserg86/EventsOfSmartContract/JavaKafkaConsumerExample.java
@@ -9,16 +9,13 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class JavaKafkaConsumerExample {
 
     public static List<String> consume() {
         String server = "localhost:9092";
-        String topicName = "topicIn";
+        String topicName = "test";
         String groupName = "test.group";
         List<String> walletAddresses = new ArrayList<>();
 
@@ -33,23 +30,52 @@ public class JavaKafkaConsumerExample {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class.getName());
 
+        // Create the consumer using props
         final Consumer<Long, String> consumer =
                 new KafkaConsumer<>(props);
 
-        TopicPartition tp = new TopicPartition(topicName, 0);
-        List<TopicPartition> tps = Arrays.asList(tp);
-        consumer.assign(tps);
-        consumer.seekToBeginning(tps);
+        // Subscribe to the topic
+        consumer.subscribe(Collections.singletonList(topicName));
 
-        ConsumerRecords<Long, String> consumerRecords = consumer.poll(30000);
-            if (!consumerRecords.isEmpty()) {
-                System.out.println("Адрес в консумер получен");
-                System.out.println("Список адресов для прослушивания: " + consumerRecords.iterator().next().value());
-                walletAddresses.add(String.valueOf(consumerRecords.iterator().next().value()));
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
             }
 
-        consumer.close();
+            walletAddresses.add(String.valueOf(consumerRecords.iterator().next().value()));
+            consumerRecords.forEach(record -> {
+                System.out.println(record.value());
+            });
 
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+
+
+
+
+
+//        TopicPartition tp = new TopicPartition(topicName, 0);
+//        List<TopicPartition> tps = Arrays.asList(tp);
+//        consumer.assign(tps);
+//        consumer.seekToBeginning(tps);
+//
+//        ConsumerRecords<Long, String> consumerRecords = consumer.poll(30000);
+//
+//            if (!consumerRecords.isEmpty()) {
+//                System.out.println("Адрес в консумер получен");
+//                System.out.println("Список адресов для прослушивания: " + consumerRecords.iterator().next().value());
+//                walletAddresses.add(String.valueOf(consumerRecords.iterator().next().value()));
+//            }
+//
+//        consumer.close();
+//
         return walletAddresses;
     }
 
